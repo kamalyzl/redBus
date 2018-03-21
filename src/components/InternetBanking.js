dbc9f813ff298c0651bfa7b46953731c8c2bc551
AlejandraCP <alejandracp710@gmail.com>
2018-03-20 19:34:04 -0500

    Show data dynamic

--- src/components/InternetBanking.js
+++ src/components/InternetBanking.js
@@ -2,11 +2,16 @@ import React from 'react'
 import SliderPay from './slider'
 import Slider from 'react-slick'
 import dataBankInternet from '../data/dataInternet.json';
+import dataInfo from '../data/informative.json';
 
 
 const imagesArray = {
   dataBankInternet
 }
+const info = {
+  dataInfo
+};
+
 const settings = {
   // dots: true,
   infinite: true,
@@ -15,9 +20,22 @@ const settings = {
   slidesToScroll: 4,
 };
 const InternetBanking = () => (
-  <Slider {...settings} className="ed-container cross-center slider">
-    <SliderPay imgArray={imagesArray.dataBankInternet.dataInternet}/>  
-  </Slider>
-) 
+  <div>
+    <p>Selecciona dónde quieres pagar</p>
+    <Slider {...settings} className="ed-container cross-center slider">
+      <SliderPay imgArray={imagesArray.dataBankInternet.dataInternet} />
+    </Slider>
+    <p>Puedes pagar desde la Web o App móvil del Banco</p>
+    <h4>Instrucciones de Pago</h4>
+    <p>
+      En esta etiqueta saldrá las instrucciones de pago que son diversas según el banco que se elija
+    </p>
+    <p>
+      2. Ingresa tu código CIP: <span>{info.dataInfo.cod}</span> y sigue los
+      pasos
+    </p>
+    <button>Ir a banca por internet</button>
+  </div>
+); 
 
 export default InternetBanking
--- src/components/PayCash.js
+++ src/components/PayCash.js
@@ -1,27 +1,45 @@
-
-
-import React from 'react'
-import SliderPay from './slider'
+import React from "react";
+import SliderPay from "./slider";
 // import $ from 'jquery'
-import Slider from 'react-slick'
-import dataCash from '../data/dataCash.json'
+import Slider from "react-slick";
+import dataCash from "../data/dataCash.json";
+import dataInfo from "../data/informative.json";
 
 const imagesArray = {
   dataCash
-}
+};
+const info = {
+  dataInfo
+};
 const settings = {
   // dots: true,
   infinite: true,
   // speed: 500,
   slidesToShow: 4,
-  slidesToScroll: 4,
-}
+  slidesToScroll: 4
+};
 
 const PayCash = () => {
   return (
-    <Slider {...settings} className="ed-container cross-center slider">
-      <SliderPay imgArray={imagesArray.dataCash.dataCash}/>  
-    </Slider>
+    <div>
+      <p>Selecciona dónde quieres pagar</p>
+      <Slider {...settings} className="ed-container cross-center slider">
+        <SliderPay imgArray={imagesArray.dataCash.dataCash} />
+      </Slider>
+      <h4>Instrucciones de Pago</h4>
+      <p>Indica que vas a realizar un pago a la empresa PAGOEFECTIVO</p>
+      <p>
+        Indica el código CIP: <span>{info.dataInfo.cod}</span> y el importe a
+        pagar en Soles
+      </p>
+      <p>
+        En esta etiqueta saldrá el costo adicional, según al banco que le de
+        click y esa información se encuntra en el json datacash en las
+        propiedades text1 y text 2, algunas estan vacías porque no tienen costo
+        adicional- solo tienen: fullcarga, bcp e interbank
+      </p>
+      <button>Encuentra tu punto de pago</button>
+    </div>
   );
 };
 export default PayCash;
