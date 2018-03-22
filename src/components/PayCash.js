d05266c9c739e57b66bfb0b19a0d4e250a51af7a
kamalyzl <kamaly.kazal@gmail.com>
2018-03-21 23:56:38 -0500

    estilo al componente PayCash

--- src/components/PayCash.js
+++ src/components/PayCash.js
@@ -23,24 +23,26 @@ const settings = {
 
 const PayCash = () => {
   return (
-    <div>
+    <div className = "ed-container selec-pay">
       <p>Selecciona dónde quieres pagar</p>
       <Slider {...settings} className='ed-container cross-center slider'>
         <SliderPay imgArray={imagesArray.dataCash.dataCash} />
       </Slider>
-      <h4>Instrucciones de Pago</h4>
-      <p>Indica que vas a realizar un pago a la empresa PAGOEFECTIVO</p>
-      <p>
-        Indica el código CIP: <span>{info.dataInfo.cod}</span> y el importe a
-        pagar en Soles
-      </p>
+      <div className="box-description-cash">
+        <p>Instrucciones de Pago</p> 
+        <ol>
+          <li><span className="number">1</span>Indica que vas a realizar un pago a la empresa PAGOEFECTIVO</li>
+          <li><span className="number">2</span>Indica el código CIP: <span>{info.dataInfo.cod}</span> y el importe a
+        pagar en Soles</li>
+        </ol>
+      </div> 
       <p>
         En esta etiqueta saldrá el costo adicional, según al banco que le de
         click y esa información se encuntra en el json datacash en las
         propiedades text1 y text 2, algunas estan vacías porque no tienen costo
         adicional- solo tienen: fullcarga, bcp e interbank
       </p>
-      <button>
+      <div className="ed-container bpi">
         <a
           href='https://ubicanos.pagoefectivo.pe/#/?tienda=[idServicio]&moneda=1&monto=100.00&ubicame=true&_k=ws2owh'
           target='_blank'
@@ -48,7 +50,8 @@ const PayCash = () => {
         >
           Encuentra tu punto de pago
         </a>
-      </button>
+      </div>
+     
     </div>
   );
 };
